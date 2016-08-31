d2e3539003f48b2cbd59644a05180a02790f8c28
Yinghua Xu <yinghua_1@yahoo.com>
2016-08-30 20:34:14 -0400

    Added support for Save button to save output String into a file

--- src/main/java/gov/nih/nci/ui/BatchProcessOutputPanel.java
+++ src/main/java/gov/nih/nci/ui/BatchProcessOutputPanel.java
@@ -3,14 +3,25 @@ package gov.nih.nci.ui;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import org.apache.log4j.Logger;
+
+import gov.nih.nci.ui.dialog.BatchProcessingDialog;
+
 public class BatchProcessOutputPanel extends JPanel implements ActionListener{
+	
+	private static final Logger log = Logger.getLogger(BatchProcessOutputPanel.class);
 
 	private static final long serialVersionUID = 1L;
 	private JTextArea textarea;
@@ -51,7 +62,26 @@ public class BatchProcessOutputPanel extends JPanel implements ActionListener{
 
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == savebutton){
-			
+			String outputStr = textarea.getText();
+			JFileChooser fc = new JFileChooser();
+			//to do - add file filter
+			int select = fc.showSaveDialog(BatchProcessOutputPanel.this);
+			if (select == JFileChooser.APPROVE_OPTION) {
+	            File file = fc.getSelectedFile();
+	            String outputfile = file.getAbsolutePath();
+	            try {
+					BufferedWriter writer =  new BufferedWriter(new FileWriter(
+							outputfile));
+					
+					writer.write(outputStr);
+					writer.close();
+					
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					log.error(e1.getMessage(), e1);
+					
+				}
+			}
 		} else if (e.getSource() == clearbutton){
 			textarea.setText(null);
 		} else if (e.getSource() == closebutton){
