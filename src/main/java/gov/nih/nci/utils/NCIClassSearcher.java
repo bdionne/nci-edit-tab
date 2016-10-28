be18b86dd65d4897d40b557d9df83a908c449866
Bob Dionne <dionne@dionne-associates.com>
2016-10-28 11:10:27 -0400

    Add modified JOptionPaneEx
    This adds a new method that allows the buttons to be overriden

--- src/main/java/gov/nih/nci/utils/NCIClassSearcher.java
+++ src/main/java/gov/nih/nci/utils/NCIClassSearcher.java
@@ -4,9 +4,10 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.util.Optional;
 
+import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-import org.protege.editor.core.ui.util.JOptionPaneEx;
+//import org.protege.editor.core.ui.util.JOptionPaneEx;
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.model.search.ClassSearcher;
 import org.semanticweb.owlapi.model.OWLClass;
@@ -24,18 +25,24 @@ public class NCIClassSearcher implements ClassSearcher {
 
 	@Override
 	public OWLClass searchFor(Component parent) {
+		
 
-
-		LuceneQueryPanel panel = new LuceneQueryPanel(oek, LuceneQueryPanel.LuceneTabLayout.VERTICAL);
+		final JButton okButton = new JButton( "OK" );
+		
+		LuceneQueryPanel panel = new LuceneQueryPanel(oek, LuceneQueryPanel.LuceneTabLayout.VERTICAL, okButton);
 		panel.setPreferredSize(new Dimension(600, 400));
-	    int response = JOptionPaneEx.showConfirmDialog(parent,
-	    		"Lucene Query Dialog", panel, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null);
+		
+		
+		                		
+	    int response = JOptionPaneEx.showDialogUsingOkButton(parent,
+	    		"Lucene Query Dialog", panel, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null, okButton);
 	    OWLClass output = null;
 	    if (response == JOptionPane.OK_OPTION) {
 	        output = panel.getSelectedEntity().asOWLClass();
 	    }	    
 	    panel.dispose();
 	    return output;
+	    
 	}
 
 }
