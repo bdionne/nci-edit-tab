d563e14e5640ec75296b2f92d223f9dbaaaf7ba6
Bob Dionne <dionne@dionne-associates.com>
2016-12-23 06:54:53 -0500

    Allow complex properties to be edited in complex edit panel

--- src/main/java/gov/nih/nci/ui/NCIOWLAnnotationsFrameSectionRow.java
+++ src/main/java/gov/nih/nci/ui/NCIOWLAnnotationsFrameSectionRow.java
@@ -1,33 +1,41 @@
 package gov.nih.nci.ui;
 
+import java.util.Set;
+
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.editor.OWLObjectEditor;
 import org.protege.editor.owl.ui.frame.OWLAnnotationsFrameSectionRow;
 import org.protege.editor.owl.ui.frame.OWLFrameSection;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
+import org.semanticweb.owlapi.model.OWLAnnotationProperty;
 import org.semanticweb.owlapi.model.OWLAnnotationSubject;
 import org.semanticweb.owlapi.model.OWLOntology;
 
 public class NCIOWLAnnotationsFrameSectionRow extends OWLAnnotationsFrameSectionRow {
 	
+	private Set<OWLAnnotationProperty> propsToExclude;
+	
 	public NCIOWLAnnotationsFrameSectionRow(OWLEditorKit owlEditorKit, 
 			OWLFrameSection<OWLAnnotationSubject, OWLAnnotationAssertionAxiom, OWLAnnotation> section, 
 			OWLOntology ontology,
-			OWLAnnotationSubject rootObject, OWLAnnotationAssertionAxiom axiom, boolean isEditable) {
+			OWLAnnotationSubject rootObject, Set<OWLAnnotationProperty> exclude, OWLAnnotationAssertionAxiom axiom, boolean isEditable) {
 		super(owlEditorKit, section, ontology, rootObject, axiom, isEditable);
+		propsToExclude = exclude;
 	}
 
 	public NCIOWLAnnotationsFrameSectionRow(OWLEditorKit owlEditorKit,
 			OWLFrameSection<OWLAnnotationSubject, OWLAnnotationAssertionAxiom, OWLAnnotation> section,
-			OWLOntology ontology, OWLAnnotationSubject rootObject, OWLAnnotationAssertionAxiom axiom) {
+			OWLOntology ontology, OWLAnnotationSubject rootObject, 
+			Set<OWLAnnotationProperty> exclude, OWLAnnotationAssertionAxiom axiom) {
 		super(owlEditorKit, section, ontology, rootObject, axiom);
+		propsToExclude = exclude;
 		// TODO Auto-generated constructor stub
 	}
 	
 	protected OWLObjectEditor<OWLAnnotation> getObjectEditor() {
         NCIOWLAnnotationEditor editor = new NCIOWLAnnotationEditor(getOWLEditorKit(),
-        		NCIEditTab.currentTab().getComplexProperties());
+        		propsToExclude);
         editor.setEditedObject(getAxiom().getAnnotation());
         return editor;
     }
