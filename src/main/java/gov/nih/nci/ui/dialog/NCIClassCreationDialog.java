38fa99ba89c25f00299fd485284bcf2a7cb5d0bd
Bob Dionne <dionne@dionne-associates.com>
2017-03-15 10:45:09 -0400

    Fix error message

--- src/main/java/gov/nih/nci/ui/dialog/NCIClassCreationDialog.java
+++ src/main/java/gov/nih/nci/ui/dialog/NCIClassCreationDialog.java
@@ -227,7 +227,7 @@ public class NCIClassCreationDialog<T extends OWLEntity> extends JPanel {
     	}
     	
     	if (!NCIEditTab.currentTab().validPrefName(preferredName)){
-    		JOptionPane.showMessageDialog(this, "Preferred name cannot contain :, !, ? or @", "Warning", JOptionPane.WARNING_MESSAGE);
+    		JOptionPane.showMessageDialog(this, "Preferred name cannot contain ! or ?", "Warning", JOptionPane.WARNING_MESSAGE);
     		return false;    		
     	}
     	
