f1d7dd54e706895eb3461379969d9c6e61c343c0
yinghua Xu <yinghua_1@yahoo.com>
2018-01-07 22:53:45 -0500

    Invoke correct dialog for non complex property in Complex Edit view

--- src/main/java/gov/nih/nci/ui/NCIOWLFrameList.java
+++ src/main/java/gov/nih/nci/ui/NCIOWLFrameList.java
@@ -3,11 +3,16 @@ package gov.nih.nci.ui;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 
+import javax.swing.JOptionPane;
 import javax.swing.event.ListSelectionListener;
 
 import org.protege.editor.core.ui.list.MListButton;
 import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.model.util.OWLAxiomInstance;
+import org.protege.editor.owl.ui.UIHelper;
+import org.protege.editor.owl.ui.axiom.AxiomAnnotationPanel;
 import org.protege.editor.owl.ui.frame.OWLFrame;
 import org.protege.editor.owl.ui.frame.OWLFrameSectionRow;
 import org.protege.editor.owl.ui.framelist.OWLFrameList;
@@ -15,7 +20,9 @@ import org.semanticweb.owlapi.model.IRI;
 import org.semanticweb.owlapi.model.OWLAnnotation;
 import org.semanticweb.owlapi.model.OWLAnnotationAssertionAxiom;
 import org.semanticweb.owlapi.model.OWLAnnotationProperty;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLClass;
+import org.semanticweb.owlapi.model.OWLOntology;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -88,48 +95,90 @@ public class NCIOWLFrameList<R> extends OWLFrameList {
     }
 	
 	public void handleAdd() {
-		loadAnnotationsAndProperties();
-		PropertyEditingDialog add = new	PropertyEditingDialog(NCIEditTabConstants.ADD, 
-				PropertyUtil.getSelectedPropertyType(annotationProps), 
-				PropertyUtil.getDefaultPropertyValues(annotationProps), 
-				PropertyUtil.getSelectedPropertyOptions(annotationProps), 
-				PropertyUtil.getDefaultSelectedPropertyLabel(annotationProps));
-		
-		HashMap<String, String> data = 	add.showDialog(this.editorKit, "Adding Properties");
-		
-		String codeStr = ((IRI)axiom.getSubject()).getShortForm();
-		OWLClass cls = NCIEditTab.currentTab().getClass(codeStr);
-		if (data != null) {
-			if (NCIEditTab.currentTab().complexPropOp(NCIEditTabConstants.ADD, cls,
-					axiom.getProperty(), axiom, data)) {
-			}				
+		if(isComplexProperty()) {
+			loadAnnotationsAndProperties();
+			PropertyEditingDialog add = new	PropertyEditingDialog(NCIEditTabConstants.ADD, 
+					PropertyUtil.getSelectedPropertyType(annotationProps), 
+					PropertyUtil.getDefaultPropertyValues(annotationProps), 
+					PropertyUtil.getSelectedPropertyOptions(annotationProps), 
+					PropertyUtil.getDefaultSelectedPropertyLabel(annotationProps));
+			
+			HashMap<String, String> data = 	add.showDialog(this.editorKit, "Adding Properties");
+			
+			String codeStr = ((IRI)axiom.getSubject()).getShortForm();
+			OWLClass cls = NCIEditTab.currentTab().getClass(codeStr);
+			if (data != null) {
+				if (NCIEditTab.currentTab().complexPropOp(NCIEditTabConstants.ADD, cls,
+						axiom.getProperty(), axiom, data)) {
+				}				
+	
+			} 
+		} else {
+			Object obj = getSelectedValue();
+	        if (!(obj instanceof OWLFrameSectionRow)) {
+	            return;
+	        }
+	        OWLFrameSectionRow<?,?,?> row = (OWLFrameSectionRow<?,?,?>) obj;
+	        OWLAxiom ax = row.getAxiom();
 
-		} 
+	        AxiomAnnotationPanel axiomAnnotationPanel;
+	            axiomAnnotationPanel = new AxiomAnnotationPanel(editorKit);
+
+	        OWLOntology ontology = row.getOntology();
+	        final OWLAxiomInstance axiomInstance;
+	        if(ontology != null) {
+	            axiomInstance = new OWLAxiomInstance(ax, ontology);
+	        }
+	        else {
+	            OWLOntology activeOntology = editorKit.getOWLModelManager().getActiveOntology();
+	            axiomInstance = new OWLAxiomInstance(ax, activeOntology);
+	        }
+	        axiomAnnotationPanel.setAxiomInstance(axiomInstance);
+	        new UIHelper(editorKit).showDialog("Annotations for " + ax.getAxiomType().toString(), axiomAnnotationPanel, JOptionPane.CLOSED_OPTION);
+	        axiomAnnotationPanel.dispose();
+		}
 		
 	}
 	
 	public void handleEdit() {
-		loadAnnotationsAndProperties();
-		if(annotationProps == null) return;
-		PropertyEditingDialog edit = new	PropertyEditingDialog(NCIEditTabConstants.EDIT, 
-				PropertyUtil.getSelectedPropertyType(annotationProps), 
-				PropertyUtil.getSelectedPropertyValues(annotations), 
-				PropertyUtil.getSelectedPropertyOptions(annotationProps), 
-				PropertyUtil.getDefaultSelectedPropertyLabel(annotationProps));
-		
-		HashMap<String, String> data = 	edit.showDialog(this.editorKit, "Editing Properties");
-		
-		String codeStr = ((IRI)axiom.getSubject()).getShortForm();
-		OWLClass cls = NCIEditTab.currentTab().getClass(codeStr);
-		if (data != null) {
-			if (NCIEditTab.currentTab().complexPropOp(NCIEditTabConstants.EDIT, cls,
-					axiom.getProperty(), axiom, data)) {
-				if (axiom.getProperty().equals(NCIEditTab.currentTab().getPreferredName())) {
-					NCIEditTab.currentTab().syncPrefName(data.get("Value"));
-				}
-			}				
-
-		} 
+		if(isComplexProperty()) {
+			loadAnnotationsAndProperties();
+			if(annotationProps == null) return;
+			PropertyEditingDialog edit = new	PropertyEditingDialog(NCIEditTabConstants.EDIT, 
+					PropertyUtil.getSelectedPropertyType(annotationProps), 
+					PropertyUtil.getSelectedPropertyValues(annotations), 
+					PropertyUtil.getSelectedPropertyOptions(annotationProps), 
+					PropertyUtil.getDefaultSelectedPropertyLabel(annotationProps));
+			
+			HashMap<String, String> data = 	edit.showDialog(this.editorKit, "Editing Properties");
+			
+			String codeStr = ((IRI)axiom.getSubject()).getShortForm();
+			OWLClass cls = NCIEditTab.currentTab().getClass(codeStr);
+			if (data != null) {
+				if (NCIEditTab.currentTab().complexPropOp(NCIEditTabConstants.EDIT, cls,
+						axiom.getProperty(), axiom, data)) {
+					if (axiom.getProperty().equals(NCIEditTab.currentTab().getPreferredName())) {
+						NCIEditTab.currentTab().syncPrefName(data.get("Value"));
+					}
+				}				
+	
+			} 
+		} else {
+			super.handleEdit();
+		}
+	}
+	
+	private boolean isComplexProperty() {
+		final Object val = getSelectedValue();
+		if (val instanceof NCIOWLAnnotationsFrameSectionRow) {
+			axiom = ((NCIOWLAnnotationsFrameSectionRow)val).getAxiom();
+			OWLAnnotationProperty annoProp = axiom.getProperty();
+			Set<OWLAnnotationProperty> complexProps = NCIEditTab.currentTab().getComplexProperties();
+			if(complexProps.contains(annoProp)) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	private void loadAnnotationsAndProperties() {
