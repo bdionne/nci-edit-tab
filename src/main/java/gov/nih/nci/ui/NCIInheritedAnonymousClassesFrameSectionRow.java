1d428e596e88351854b8f8b1b191147856a26c21
Bob Dionne <dionne@dionne-associates.com>
2018-08-08 19:07:16 -0400

    WIP - handling of anonymous classes

--- src/main/java/gov/nih/nci/ui/NCIInheritedAnonymousClassesFrameSectionRow.java
+++ src/main/java/gov/nih/nci/ui/NCIInheritedAnonymousClassesFrameSectionRow.java
@@ -1,24 +1,36 @@
 package gov.nih.nci.ui;
 
 import java.util.Arrays;
+
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
+import org.protege.editor.core.ProtegeApplication;
+import org.protege.editor.core.prefs.Preferences;
+import org.protege.editor.core.prefs.PreferencesManager;
 import org.protege.editor.owl.OWLEditorKit;
 import org.protege.editor.owl.ui.editor.OWLObjectEditor;
 import org.protege.editor.owl.ui.frame.AbstractOWLFrameSectionRow;
 import org.protege.editor.owl.ui.frame.OWLFrameSection;
+import org.protege.editor.owl.ui.preferences.GeneralPreferencesPanel;
 import org.semanticweb.owlapi.model.AxiomType;
 import org.semanticweb.owlapi.model.OWLClass;
 import org.semanticweb.owlapi.model.OWLClassAxiom;
 import org.semanticweb.owlapi.model.OWLClassExpression;
 import org.semanticweb.owlapi.model.OWLEquivalentClassesAxiom;
+import org.semanticweb.owlapi.model.OWLObject;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLSubClassOfAxiom;
 import org.semanticweb.owlapi.util.CollectionFactory;
 
 public class NCIInheritedAnonymousClassesFrameSectionRow extends AbstractOWLFrameSectionRow<OWLClass, OWLClassAxiom, OWLClassExpression> {
+	
+	private String dispAnon = PreferencesManager.getInstance().getApplicationPreferences(ProtegeApplication.ID)
+			.getString(GeneralPreferencesPanel.ANON_ANCESTORS_DISPLAY, 
+					GeneralPreferencesPanel.AnonDispNone);
+	
 	public NCIInheritedAnonymousClassesFrameSectionRow(OWLEditorKit owlEditorKit, OWLFrameSection<OWLClass, OWLClassAxiom, OWLClassExpression> section,
 	            OWLOntology ontology, OWLClass rootObject, OWLClassAxiom axiom) {
 		super(owlEditorKit, section, ontology, rootObject, axiom);
@@ -76,6 +88,23 @@ public class NCIInheritedAnonymousClassesFrameSectionRow extends AbstractOWLFram
 			return Arrays.asList(descs.iterator().next());
 		}
 	}
+	
+	public String getRendering() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getPrefix());
+        for (Iterator<? extends OWLObject> it = getManipulatableObjects().iterator(); it.hasNext();) {
+            OWLObject obj = it.next();
+            sb.append(getObjectRendering(obj));
+            if (it.hasNext()) {
+                sb.append(getDelimeter());
+            }
+        }
+        if (dispAnon == GeneralPreferencesPanel.AnonDispAll) {
+        	sb.append(" (" + getObjectRendering(getRootObject()) + ")");
+        }
+        sb.append(getSuffix());
+        return sb.toString();
+    }
 	
 	
 	public String getTooltip() {
